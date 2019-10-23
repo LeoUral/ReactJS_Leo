43e968e814ad566801e4f63a665c34dc31a70afe
Leonid <49805034+LeoUral@users.noreply.github.com>
2019-10-24 03:44:59 +0500

    Add files via upload

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -1,53 +1,60 @@
-//Lesson-1 
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { script } from './script.js';
-
-// Компонент кнопки для HTML
-const ButtonComponent = () => {
-    return (
-        <button className="btn">Жми на меня</button>
-    );
-}
-
-const messages = ['Привет!', 'Как дела?'];
-const MessageComponent = (props) => <div className="messages"> {props.text} </div>;
-const MessageField = (props) => <div>
-    <h1>Чат</h1>
-    {props.messages.map(message => <MessageComponent text={message} />)}
-    <ButtonComponent />
-</div>;
+import MessageField from './MessageField.jsx';
 
 ReactDOM.render(
-    <MessageField messages={messages} />,
-    document.getElementById('root') ,
+    <MessageField />,
+    document.getElementById('root'),
 );
 
-// обработка нажатия кнопки
-let button = document.querySelector('.btn');
-button.addEventListener('click', function (event) {
-    let newlength = messages.push('Нормально');
+// import { script } from './script.js';
 
-    ReactDOM.render(
-        <MessageField messages={messages} />,
-        document.getElementById('root') ,
-    );
-});
+// // Урок номер 1 ниже
+// // Компонент кнопки для HTML
+// const ButtonComponent = () => {
+//     return (
+//         <button className="btn">Жми на меня</button>
+//     );
+// }
 
-// const element = React.createElement(
-//     'h1',
-//     { className: "element" },
-//     'Кажется, мы подключили React' ,
-// );
+// const messages = ['Привет!', 'Как дела?'];
+// const MessageComponent = (props) => <div className="messages"> {props.text} </div>;
+// const MessageField = (props) => <div>
+//     <h1>Чат</h1>
+//     {props.messages.map(message => <MessageComponent text={message} />)}
+//     <ButtonComponent />
+// </div>;
 
-// const element = <h1 className="element">Кажется, мы подключили React</h1>;
 // ReactDOM.render(
-//     element, document.getElementById('root'),
+//     <MessageField messages={messages} />,
+//     document.getElementById('root') ,
 // );
 
-// const content = 'Кажется, мы подключили React еще раз';
-// const Component = (props) => <h1 className="element">{props.content}</h1>;
-// ReactDOM.render(
-//     <Component content={content} />,
-//     document.getElementById('root'),
-// );
+// // обработка нажатия кнопки
+// let button = document.querySelector('.btn');
+// button.addEventListener('click', function (event) {
+//     let newlength = messages.push('Нормально!');
+
+//     ReactDOM.render(
+//         <MessageField messages={messages} />,
+//         document.getElementById('root') ,
+//     );
+// });
+
+// // const element = React.createElement(
+// //     'h1',
+// //     { className: "element" },
+// //     'Кажется, мы подключили React' ,
+// // );
+
+// // const element = <h1 className="element">Кажется, мы подключили React</h1>;
+// // ReactDOM.render(
+// //     element, document.getElementById('root'),
+// // );
+
+// // const content = 'Кажется, мы подключили React еще раз';
+// // const Component = (props) => <h1 className="element">{props.content}</h1>;
+// // ReactDOM.render(
+// //     <Component content={content} />,
+// //     document.getElementById('root'),
+// // );
